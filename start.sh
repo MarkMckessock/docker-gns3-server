407b357353b05ad19365d7c446876144d71f0963
Mark Mckessock <mark.mckessock@gmail.com>
2022-09-07 18:53:13 -0400

    Change startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "${CONFIG}x" == "x" ]; then
+if [ "${CONFIG}x" -eq "x" ]; then
 	CONFIG=/data/config.ini
 fi
 
@@ -7,14 +7,4 @@ if [ ! -e $CONFIG ]; then
 	cp /config.ini /data
 fi
 
-brctl addbr virbr0
-ip link set dev virbr0 up
-if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
-  BRIDGE_ADDRESS=172.21.1.1/24
-fi
-ip ad add ${BRIDGE_ADDRESS} dev virbr0
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
-dockerd --storage-driver=vfs --data-root=/data/docker/ &
 gns3server -A --config /data/config.ini
